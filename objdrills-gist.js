a19570b410133a863ca3917e62a8875485210a36
L Barbee <bluelb84@gmail.com>
2017-07-19 15:32:25 -0400

    drill #3 solution

--- objdrills-gist.js
+++ objdrills-gist.js
@@ -1,1 +1,43 @@
-// something should go here
+// **********  1. Object initializers and methods  ************
+
+const loaf = {
+    flour: 300,
+    water: 210
+}
+
+// console.log(loaf.flour);
+// console.log(loaf.water);
+
+loaf.hydration = function(){};
+// console.log(loaf);
+
+loaf.hydration = function(water, flour) {
+    return (this.water/this.flour)*100;
+}
+
+console.log(loaf.hydration());
+
+
+
+//  ********* 2. Iterating over an object's properties  *************
+
+const objDrill = {
+    foo: 1,
+    bar: 2,
+    fum: 3,
+    quux: 4,
+    spam: 5
+}
+
+// for(key in objDrill) {
+//     console.log(key, objDrill[key]);
+// }
+
+
+//  ********  3. Arrays in objects  *********
+
+const obj = {
+    meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+}
+
+console.log(obj.meals[3]);
