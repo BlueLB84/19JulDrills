d2f32d086f445fadab3df9ae7cf6367e3058d176
L Barbee <bluelb84@gmail.com>
2017-07-19 17:02:44 -0400

    Properties that aren't there solution

--- objdrills-gist.js
+++ objdrills-gist.js
@@ -15,7 +15,7 @@ loaf.hydration = function(water, flour) {
     return (this.water/this.flour)*100;
 }
 
-console.log(loaf.hydration());
+// console.log(loaf.hydration());
 
 
 
@@ -34,10 +34,61 @@ const objDrill = {
 // }
 
 
+
 //  ********  3. Arrays in objects  *********
 
 const obj = {
     meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
 }
 
-console.log(obj.meals[3]);
+// console.log(obj.meals[3]);
+
+
+
+//  ********  4. Arrays of objects  **********
+
+const person1 = {
+    name: 'Zoe',
+    job_title: 'Top Little Dog'
+}
+
+const person2 = {
+    name: 'Chief',
+    job_title: 'Top Big Dog'
+}
+
+const person3 = {
+    name: 'Lindsey',
+    job_title: 'Top Dog Walker'
+}
+
+let arrOfPeople = [person1, person2, person3];
+
+// for (let i = 0; i < arrOfPeople.length; i++) {
+//     console.log(`${arrOfPeople[i].name} is the ${arrOfPeople[i].job_title}`);
+// }
+
+// arrOfPeople.forEach(function(item){
+//     console.log(item);
+// });
+
+
+
+//  ***********  5. Properties that aren't there  ************
+
+arrOfPeople.forEach(function(item){
+    if (item.name !== 'Lindsey') {
+        item.boss = 'Lindsey';
+    } 
+    if (item.boss === undefined) {
+        console.log(`${item.job_title} ${item.name} doesn't report to anybody`);
+    } else {
+        console.log(`${item.job_title} ${item.name} reports to ${item.boss}`);
+    }
+});
+
+
+console.log(person1);
+
+
+
